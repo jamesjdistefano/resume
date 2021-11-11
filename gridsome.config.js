bfa63242c159ce9a4324896fd786a50a50eda7e6
jamesjdistefano <jamesjdistefano@gmail.com>
2021-11-11 10:27:23 -0500

    Updates gridsome.config

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: 'James J. DiStefano',
+  siteUrl: 'https://jamesjdistefano.github.io/resume/',
+  pathPrefix: '/resume'
 }
